4e06a26a1f5917e8387c60a17cab868a7451dc1b
DenisGerasimenko <gerasimenkodenis7@gmail.com>
2023-05-30 15:50:17 +0300

    add a layout of costs project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Costs from './components/Costs/Costs.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const costs = [
+        {
+            date: new Date(2021, 2, 12),
+            description: 'Холодльник',
+            amount: 999.99
+        },
+        {
+            date: new Date(2021, 11, 25),
+            description: 'MacBook',
+            amount: 1254.72
+        },
+        {
+            date: new Date(2021, 1, 4),
+            description: 'Джинсы',
+            amount: 49.99
+        }
+    ]
+
+    return (
+        <div>
+            <h1>Изучение Реакт</h1>
+            <Costs costs={costs}/>
+
+        </div>
+    );
 }
 
 export default App;
