5899773075bf519077f0af757929fb74822d57ec
DenisGerasimenko <gerasimenkodenis7@gmail.com>
2023-07-31 18:52:28 +0300

    add diagram of costs

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,8 +1,9 @@
 import "./Costs.css";
-import CostItem from "./CostItem";
 import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
 import React, {useState} from "react";
+import CostList from "./CostList";
+import CostsDiagram from "./CostsDiagram";
 
 const Costs = (props) => {
     const [selectedYear, setSelectedYear] = useState("2021");
@@ -15,24 +16,13 @@ const Costs = (props) => {
         return cost.date.getFullYear().toString() === selectedYear
     })
 
-    let costsContent = <p>В этом году расходов нет</p>
-
-    if (filteredCosts.length > 0) {
-        costsContent = filteredCosts.map((cost) => (
-            <CostItem
-                key={cost.id}
-                date={cost.date}
-                description={cost.description}
-                amount={cost.amount}
-            />
-        ))
-    }
 
     return (
         <div>
             <Card className="costs">
                 <CostsFilter onChangeYear={yearChangeHandler} year={selectedYear}/>
-                {costsContent}
+                <CostsDiagram costs={filteredCosts}/>
+                <CostList costs={filteredCosts}/>
             </Card>
         </div>
     );
--- src/components/NewCost/NewCost.js
+++ src/components/NewCost/NewCost.js
@@ -1,20 +1,35 @@
+import React, {useState} from "react";
 import "./NewCost.css";
 import CostForm from "./CostForm";
 
 const NewCost = (props) => {
-  const saveCostDataHandler = (inputCostData) => {
-    const costData = {
-      ...inputCostData,
-      id: Math.random().toString(),
-    };
-    props.onAddCost(costData);
-  };
 
-  return (
-    <div className="new-cost">
-      <CostForm onSaveCostData={saveCostDataHandler} />
-    </div>
-  );
+    const [isFormVisible, setIsFormVisible] = useState(false)
+
+    const saveCostDataHandler = (inputCostData) => {
+        const costData = {
+            ...inputCostData,
+            id: Math.random().toString(),
+        };
+        props.onAddCost(costData);
+        setIsFormVisible(false)
+    };
+
+
+    const inputCostDataHandler = () => {
+        setIsFormVisible(true)
+    }
+
+    const cancelCostHandler = () => {
+        setIsFormVisible(false)
+    }
+
+    return (
+        <div className="new-cost">
+            {!isFormVisible && <button onClick={inputCostDataHandler}>Добавить новый расход</button>}
+            {isFormVisible && <CostForm onSaveCostData={saveCostDataHandler} onCancel={cancelCostHandler}/>}
+        </div>
+    );
 };
 
 export default NewCost;
